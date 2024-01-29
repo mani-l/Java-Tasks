9efac145f8b0fc328536f90995dd7c0381625731
mani-l <151550000+mani-l@users.noreply.github.com>
2024-01-29 17:45:56 +0530

    Data Types

---
+++ Datatypes.java
@@ -0,0 +1,34 @@
+public class Datatypes{
+	
+	public static void main(String[] args){
+		
+		//String - stores group of characters
+		String n = "Manikandan";
+		
+		//char - stores single character, letter, symbol
+		char c = 'L';
+		
+		//Numeric datatype
+		byte b = -6;
+		short s = 618;
+		int i = 82823;
+		long l = 636936506;
+		
+		//Decimal
+		float f = 18.6f;
+		double d = 1459.08553;
+		
+		//Boolean
+		boolean t = false;
+		
+		System.out.println(n);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(t);
+	}
+}
