d3a533cf687f5c884e56544294a0884b559abec7
mani-l <151550000+mani-l@users.noreply.github.com>
2024-02-03 23:03:31 +0530

    Relationaloperation

---
+++ 7.Relationaloperation/Relational.java
@@ -0,0 +1,30 @@
+class Relational{
+	public static void main(String[] args) {
+
+		int a=1,b=0;
+
+			if(a>b){
+			System.out.println("a is greater than b");
+			}
+			else if(a<b){
+				System.out.println("a is lesser than b");
+
+			}
+			else if(a==b){
+				System.out.println(a==b);
+
+			}
+			else if(a!=b){
+				System.out.println("a not equal to b");
+
+			}
+			else if(a>=b){
+				System.out.println("a is greater than equal to b");
+
+			}
+			else if(a<=b){
+				System.out.println("a is lesser than equal to  b");
+}
+			}
+			
+	}
